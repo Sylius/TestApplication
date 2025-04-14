22fa9b1db5fbc5c419d530a2032595f0b63ddc11
Grzegorz Sadowski <sadowskigp@gmail.com>
2025-04-14 15:33:48 +0200

    Use  + fix order of imported configs

--- src/Kernel.php
+++ src/Kernel.php
@@ -66,12 +66,6 @@ final class Kernel extends BaseKernel
             $valuePreProcessor = AbstractConfigurator::$valuePreProcessor;
             AbstractConfigurator::$valuePreProcessor = fn ($value) => $this === $value ? new Reference('kernel') : $value;
 
-            if (isset($_SERVER['CONFIGS_TO_IMPORT'])) {
-                foreach (explode(';', $_SERVER['CONFIGS_TO_IMPORT']) as $filePath) {
-                    $kernelLoader->import($filePath);
-                }
-            }
-
             try {
                 $configureContainer->getClosure($this)(new ContainerConfigurator($container, $kernelLoader, $instanceof, $file, $file, $this->getEnvironment()), $loader, $container);
             } finally {
@@ -82,7 +76,11 @@ final class Kernel extends BaseKernel
 
             $container->setAlias($kernelClass, 'kernel')->setPublic(true);
 
-
+            if (isset($_SERVER['CONFIGS_TO_IMPORT'])) {
+                foreach (explode(';', $_SERVER['CONFIGS_TO_IMPORT']) as $filePath) {
+                    $kernelLoader->import($filePath);
+                }
+            }
         });
 
     }
